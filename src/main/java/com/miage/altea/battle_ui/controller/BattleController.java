24b54829da6e30bf2ef24b086298647e4da29440
axellecess <axellecess@gmai.com>
2020-03-22 14:17:14 +0100

    [refactor]

--- src/main/java/com/miage/altea/battle_ui/controller/BattleController.java
+++ src/main/java/com/miage/altea/battle_ui/controller/BattleController.java
@@ -1,5 +1,8 @@
 package com.miage.altea.battle_ui.controller;
 
+import com.miage.altea.battle_ui.battle.Battle;
+import com.miage.altea.battle_ui.service.BattleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +13,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class BattleController {
 
+    @Autowired
+    BattleService battleService;
+
     @PostMapping(value = "/battles")
     public ModelAndView createBattle(@RequestParam(value = "trainer", required = false) String trainer,
                                      @RequestParam(value = "opponent", required = false) String opponent){
+        Battle battle = battleService.createBattle(trainer, opponent);
         return null;
     }
 
--- src/main/java/com/miage/altea/battle_ui/service/BattleService.java
+++ src/main/java/com/miage/altea/battle_ui/service/BattleService.java
@@ -3,6 +3,7 @@ package com.miage.altea.battle_ui.service;
 import com.miage.altea.battle_ui.battle.Battle;
 import com.miage.altea.battle_ui.battle.BattleTrainer;
 import com.miage.altea.battle_ui.trainers.bo.Trainer;
+import com.miage.altea.battle_ui.trainers.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,17 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class BattleService {
 
-    public Battle createBattle(){
+    @Autowired
+    TrainerService trainerService;
+
+    public Battle createBattle(String trainerName, String opponentName){
         Battle battle = new Battle();
+
+        BattleTrainer trainer = trainerService.getTrainer(trainerName);
+        BattleTrainer opponent = trainerService.getTrainer(opponentName);
+
+        battle.setTrainer(trainer);
+        battle.setOpponent(opponent);
 
         return  battle;
     }
